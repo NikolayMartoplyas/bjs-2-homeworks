7c9076f663128e7ade93240595419f21a5e379f9
Nikolay Martoplyas <kolyan94martoplyas@gmail.com>
2024-10-10 15:25:36 +0300

    программу для решения квадратных уравнений

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	const discriminat = b ** 2 - 4 * a * c;
+
+	if (discriminat < 0) {
+		return arr;
+	}
+	if (discriminat === 0) {
+		return arr [-b / (2 * a)];
+	} else if (discriminat > 0) {
+		return arr [
+			(-b + Math.sqrt(d)) / (2 * a),
+			(-b - Math.sqrt(d)) / (2 * a)
+		]
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
