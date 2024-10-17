349acceaf203c2bc6de4ac62bef747d35423fbf4
Nikolay Martoplyas <kolyan94martoplyas@gmail.com>
2024-10-17 16:52:00 +0300

    Выполнено дз 3 Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,32 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	if (arr1.every((element, index) => element === arr2[index])) {
+		return true
+	} else {
+		return false;
+	}
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users
+		.filter(user => user.gender === gender)
+		.map(user => user.age)
+		.reduce((acc, age, index, array) => {
+			acc += age;
+			if (index === array.length - 1) {
+				return acc / array.length;
+			}
+			return acc;
+		}, 0);
+
+	if (users.length === 0) {
+		return 0;
+	}
+	if (gender !== "мужской" || gender !== "женский") {
+		return 0;
+	}
+
 }
